1ece4b54f0c318317663dc6ef507dec4759690a2
zc97 <zc312060172@gmail.com>
2020-12-28 13:25:25 +0000

    Fixed the comparison result not consistent bug

--- linear_programming_test_model/TestAgent.java
+++ linear_programming_test_model/TestAgent.java
@@ -111,7 +111,7 @@ public class TestAgent extends AbstractNegotiationParty {
 
             // Create empty model
             GRBModel model = new GRBModel(env);
-
+            model.set(GRB.IntParam.NonConvex, 2);
             //create a data struct using hash map with pattern likes:
 
             //        -> value1 -> variable 1,1
@@ -130,12 +130,18 @@ public class TestAgent extends AbstractNegotiationParty {
                 IssueDiscrete discreteIssue = (IssueDiscrete) issue;
                 List<ValueDiscrete> discreteValues = discreteIssue.getValues();
                 HashMap<ValueDiscrete, String> valueVarTable = new HashMap<>();
+                String weightName = "w" + issueNum.toString();
 
-                //iterate every value and put in to valueVarTable
+                //Adding weight variables for each issue, name: w1, w2, w3 ...
+                model.addVar(0.0, 1.0, 0.0, GRB.CONTINUOUS, weightName);
+                model.update();
+                System.out.println(weightName + " " + issue.getName());
+                //iterate every value and put in to valueVarTable, name: u11, u12, u21, u22 ...
                 for (ValueDiscrete value : discreteValues){
                     //get the index of values, adding 1 because we want the index to start at 1 not 0
                     Integer valueNum = ((IssueDiscrete) issue).getValueIndex(value) + 1;
                     String varName = "u" + issueNum.toString() + valueNum.toString();
+                    System.out.println(varName + " " + value.getValue());
                     model.addVar(0.0, 1.0, 0.0, GRB.CONTINUOUS, varName);
                     model.update();
                     valueVarTable.put(value, varName);
@@ -144,7 +150,7 @@ public class TestAgent extends AbstractNegotiationParty {
             }
 
             //check all variables by printing
-            GRBVar[] vars = model.getVars();
+//            GRBVar[] vars = model.getVars();
 //            System.out.println("**************** All Variables ****************");
 //            for(GRBVar var:vars){
 //                System.out.println(var.get(GRB.StringAttr.VarName));
@@ -166,9 +172,9 @@ public class TestAgent extends AbstractNegotiationParty {
             //bid2 is offering value1 for issue1, value3 for issue2
             //bid1 is offering value2 for issue1, value2 for issue2
             //a constraint can be constructed accordingly:
-            //u11 + u23 >= u12 + u22
+            //w1*u11 + w2*u23 >= w1*u12 + w2*u22
             //For the purpose of optimization, e is added to get maximize the margin between the utility bid1 and bid2:
-            //u11 + u23 >= u12 + u22 + e
+            //w1*u11 + w2*u23 >= w1*u12 + w2*u22 + e
 
             //get all partial ordered bid comparisons
             //bid1 > bid2 returns -1
@@ -176,15 +182,43 @@ public class TestAgent extends AbstractNegotiationParty {
             System.out.println("**************** Adding Constraints ****************");
             List<OutcomeComparison> comparisonsList = r.getPairwiseComparisons();
 
+
+            //After checking the comparison result relationship  (-1,1,>=,<=), found out the comparison result is the other way around!
+//            OutcomeComparison c = comparisonsList.get(10);
+//
+//            System.out.println(comparisonsList.size());
+//            Bid b1 = c.getBid1();
+//            Bid b2 = c.getBid2();
+//            int cr = c.getComparisonResult();
+//            System.out.println("**************** Result ****************");
+//
+//            System.out.println("Result: " + cr);
+//
+//            System.out.println("**************** Bid 1 ****************");
+//
+//            for (Issue issue : issues) {
+//                IssueDiscrete discreteIssue = (IssueDiscrete) issue;
+//                System.out.print(discreteIssue.getName() + ",");
+//                System.out.print(b1.getValue(discreteIssue).toString() + ",");
+//            }
+//
+//            System.out.println("**************** Bid 2 ****************");
+//            for (Issue issue : issues) {
+//                IssueDiscrete discreteIssue = (IssueDiscrete) issue;
+//                System.out.print(discreteIssue.getName() + ",");
+//                System.out.print(b2.getValue(discreteIssue).toString() + ",");
+//            }
+
             Integer index = 0;
             for(OutcomeComparison comparison:comparisonsList){
+
                 String comparisonName = "c" + index.toString();
                 index ++;
                 Bid bid1 = comparison.getBid1();
                 Bid bid2 = comparison.getBid2();
                 int result = comparison.getComparisonResult();
-                GRBLinExpr expr1 = new GRBLinExpr();
-                GRBLinExpr expr2 = new GRBLinExpr();
+                GRBQuadExpr expr1 = new GRBQuadExpr();
+                GRBQuadExpr expr2 = new GRBQuadExpr();
                 for (Issue issue : issues){
                     Integer issueNum = issue.getNumber();
                     IssueDiscrete discreteIssue = (IssueDiscrete) issue;
@@ -194,25 +228,31 @@ public class TestAgent extends AbstractNegotiationParty {
                     Integer valueNum2 = discreteIssue.getValueIndex((ValueDiscrete) value2) + 1;
                     String varName1 = "u" + issueNum.toString() + valueNum1.toString();
                     String varName2 = "u" + issueNum.toString() + valueNum2.toString();
-                    expr1.addTerm(1.0, model.getVarByName(varName1));
-                    expr2.addTerm(1.0, model.getVarByName(varName2));
+                    String issueName = "w" + issueNum.toString();
+                    expr1.addTerm(1.0, model.getVarByName(issueName), model.getVarByName(varName1));
+                    expr2.addTerm(1.0, model.getVarByName(issueName), model.getVarByName(varName2));
                 }
-                char comparisonPara = GRB.GREATER_EQUAL;
-                if(result == 1){
+                char comparisonPara;
+                if(result == -1){
                     comparisonPara = GRB.LESS_EQUAL;
                     expr1.addTerm(1.0, e);
                 }else{
                     comparisonPara = GRB.GREATER_EQUAL;
                     expr2.addTerm(1.0, e);
                 }
-                model.addConstr(expr1, comparisonPara, expr2, comparisonName);
+                model.addQConstr(expr1, comparisonPara, expr2, comparisonName);
             }
 
             //adding constraints that make sure the summation of values from the same issue is exact 1
+            String weightSumTo1name = "weightSumTo1";
+            GRBLinExpr exprWeightSumTo1 = new GRBLinExpr();
             for (Issue issue : issues) {
                 String sumTo1name = issue.getName() + "sumTo1";
                 IssueDiscrete discreteIssue = (IssueDiscrete) issue;
                 Integer issueNum = issue.getNumber();
+                String weightName = "w" + issueNum.toString();
+                exprWeightSumTo1.addTerm(1.0, model.getVarByName(weightName));
+
                 List<ValueDiscrete> discreteValues = discreteIssue.getValues();
                 GRBLinExpr exprSumTo1 = new GRBLinExpr();
                 for (ValueDiscrete value : discreteValues){
@@ -222,8 +262,8 @@ public class TestAgent extends AbstractNegotiationParty {
                 }
                 model.addConstr(exprSumTo1, GRB.EQUAL, 1.0, sumTo1name);
             }
-
-            model.addConstr(e, GRB.GREATER_EQUAL, 0.001, "epsilon");
+            model.addConstr(exprWeightSumTo1, GRB.EQUAL, 1.0,weightSumTo1name);
+            model.addConstr(e, GRB.GREATER_EQUAL, 0, "epsilon");
 
             // Optimize model
             model.optimize();
@@ -232,6 +272,8 @@ public class TestAgent extends AbstractNegotiationParty {
             for (Issue issue : issues) {
                 Integer issueNum = issue.getNumber();
                 IssueDiscrete discreteIssue = (IssueDiscrete) issue;
+                String issueName = "w" + issue.getNumber();
+                System.out.println(issueName + " " + model.getVarByName(issueName).get(GRB.DoubleAttr.X));
                 List<ValueDiscrete> discreteValues = discreteIssue.getValues();
                 for (ValueDiscrete value : discreteValues){
                     Integer valueNum = ((IssueDiscrete) issue).getValueIndex(value) + 1;
